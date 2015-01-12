09ae3fe496a1b596a3bac84e082c5b220d86f678
bulte <bulte@polytech.unice.fr>
2015-01-12 23:02:49 +0100

    Home page draft

--- TroisSurQuatre/app/src/main/java/com/example/bu/troissurquatre/MainActivity.java
+++ TroisSurQuatre/app/src/main/java/com/example/bu/troissurquatre/MainActivity.java
@@ -4,14 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 
+    Toast mToast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToast = Toast.makeText( this  , "" , Toast.LENGTH_SHORT );
+        addButtonListener();
     }
 
 
@@ -35,5 +42,63 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+    public void addButtonListener() {
+
+
+        final Button button_maison = (Button) findViewById(R.id.button_maison);
+        button_maison.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "MAISON", Toast.LENGTH_SHORT).show();
+                mToast.setText( "MAISON" );
+                mToast.show();
+            }
+        });
+
+        final Button button_saisons = (Button) findViewById(R.id.button_saisons);
+        button_saisons.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "4 SAISONS", Toast.LENGTH_SHORT).show();
+                mToast.setText( "4 SAISONS" );
+                mToast.show();
+            }
+        });
+
+        final Button button_shopping = (Button) findViewById(R.id.button_shopping);
+        button_shopping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "SHOPPING", Toast.LENGTH_SHORT).show();
+                mToast.setText( "SHOPPING" );
+                mToast.show();
+            }
+        });
+
+        final Button button_promenade = (Button) findViewById(R.id.button_promenade);
+        button_promenade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "PROMENADE", Toast.LENGTH_SHORT).show();
+                mToast.setText( "PROMENADE" );
+                mToast.show();
+            }
+        });
+
+        final Button button_aleatoire = (Button) findViewById(R.id.button_aleatoire);
+        button_aleatoire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "ALEATOIRE", Toast.LENGTH_SHORT).show();
+                mToast.setText( "ALEATOIRE" );
+                mToast.show();
+            }
+        });
     }
 }
